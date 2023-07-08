ef8e2a32f60b3c4d24d5de3d4cc3e14216fe37e9
AzamatAbraev <azamatabraev07@gmail.com>
2023-07-09 01:29:32 +0500

    header and footer added

---
+++ JS/main.js
@@ -0,0 +1,11 @@
+function openNavbar() {
+  document.getElementById("navbar-responsive").style.right = "0";
+  // document.getElementById("navbar-responsive").style.top = "0";
+}
+function closeNavbar() {
+  document.getElementById("navbar-responsive").style.right = "-100%";  
+  // document.getElementById("navbar-responsive").style.top = "-100%";
+}
+
+document.getElementById("navbar-open").addEventListener("click", openNavbar);
+document.getElementById("navbar-close").addEventListener("click", closeNavbar);
